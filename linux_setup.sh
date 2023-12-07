9159c1a2a9b31b580498f4d0a13c51c7d3d51329
viettr <viettran295@gmail.com>
2023-12-07 22:26:05 +0100

    Update setup and add vscode settings

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
 sudo apt update && sudo apt upgrade -y
 sudo apt install build-essential -y
+
+# setup git
 sudo apt install git -y
+sudo apt install tig -y
+git config --global user.name="viettr"
+git config --global user.email "viettran295@gmail.com"
+# git remote set-url origin https://<username>:<personal_token>@github.com/viettran295/<repo_name>.git
+
+# setup go
+sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
