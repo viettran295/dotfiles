873be645ac49e7dd8bcd1fc22b6c0a8a6fedafbe
viettr <viettran295@gmail.com>
2024-01-23 03:11:11 +0100

    Add setup tieng viet

--- linux_setup.sh
+++ linux_setup.sh
@@ -54,3 +54,11 @@ sudo make install
 # install stm32cudeide (optional)
 # bash st-stm32cubeide_1.14.0_19471_20231121_1200_amd64.sh
 # sudo apt-get install libncurses5 -y
+
+#Cài đặt tiếng việt
+sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
+sudo apt-get update
+sudo apt-get install ibus ibus-bamboo --install-recommends
+ibus restart
+# Đặt ibus-bamboo làm bộ gõ mặc định
+env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
