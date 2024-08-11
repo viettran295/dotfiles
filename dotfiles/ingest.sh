43bad2b37f1c4f997dd18c707280f3c5e84f362a
viettr <viettran295@gmail.com>
2024-08-11 23:21:49 +0200

    Add config for lf File manager

--- dotfiles/ingest.sh
+++ dotfiles/ingest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SRC=(~/.config/nvim ~/.config/polybar /etc/conky) 
+SRC=(~/.config/nvim ~/.config/polybar ~/.config/lf /etc/conky) 
 
 for srcPath in "${SRC[@]}"; do 
      sudo cp -r $srcPath ./
