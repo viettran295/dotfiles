87f422458e914910b8d2031940b80d390f8784f2
viettr <viettran295@gmail.com>
2025-03-31 21:56:32 +0200

    Add conf kitty yazi

--- .config/ingest.sh
+++ .config/ingest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SRC=(~/.config/nvim ~/.config/polybar ~/.bashrc) 
+SRC=(~/.config/kitty ~/.config/yazi ~/.config/nvim ~/.config/polybar ~/.bashrc) 
 
 for srcPath in "${SRC[@]}"; do 
      sudo cp -r $srcPath ./
